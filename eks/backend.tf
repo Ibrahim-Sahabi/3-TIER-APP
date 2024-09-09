2b03b25498e6bfd643e8f25af7b590a8dce97e98
Ibrahim-Sahabi <isahabi@yahoo.com>
2024-09-09 11:12:41 -0400

    updated eks and modules files

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,20 +1,22 @@
 terraform {
-  required_version = "~> 1.9.3"
+  #required_version = "~> 1.9.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.49.0"
     }
   }
-  backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
-    key            = "eks/terraform.tfstate"
+}
+
+  terraform { 
+    backend "s3" {
+    bucket         = "teamusa-test-bucket"
+    region         = "us-east-2"
+    key            = "eks3/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
 }
-
 provider "aws" {
   region  = var.aws-region
 }
